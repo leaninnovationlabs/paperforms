adc56e7249b3e6d12c05be78a0dfa80eee19fba3
Camilo Calvo Alcaniz <camiloa@leaninnovationlabs.com>
2025-01-28 10:03:42 -0500

    client: Improved header style, branded it with the LIL logo

--- client/src/features/Upload.page.tsx
+++ client/src/features/Upload.page.tsx
@@ -1,12 +1,19 @@
+import Logo from "../components/logo/Logo.component";
 import "./Upload.page.css";
 
 const UploadPage = () => {
   return (
     <div className="layout">
-      <div className="title">Tax Form Validator</div>
+      <div className="header">
+        <Logo />
+        <div className="title">Tax Form Validator</div>
+        <div className="empty-space"></div>
+      </div>
       <main className="main-content">
         <input type="file" id="file" hidden name="file" accept=".pdf" />
-        <label htmlFor="file" className="file-input">Upload Here</label>
+        <label htmlFor="file" className="file-input">
+          Upload Here
+        </label>
         <div className="validate-button">Validate!</div>
       </main>
     </div>
