614dfcf0a4b02559f60eaa7ce78b231f65a64eb2
Camilo Calvo Alcaniz <camiloa@leaninnovationlabs.com>
2025-01-28 10:32:56 -0500

    client: Added select for file type and button to customize rules

--- client/src/features/Upload.page.tsx
+++ client/src/features/Upload.page.tsx
@@ -1,7 +1,24 @@
+import Hamburger from "../components/hamburger/Hamburger.component";
 import Logo from "../components/logo/Logo.component";
 import "./Upload.page.css";
 
 const UploadPage = () => {
+  const renderSelect = () => {
+    return (
+      <div className="select-area">
+        <div className="select-container">
+          <select className="select-input">
+            <option value="w9">W9</option>
+            <option value="w2">W2</option>
+          </select>
+        </div>
+        <div className="rules-button">
+          <Hamburger />
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className="layout">
       <div className="header">
@@ -10,6 +27,7 @@ const UploadPage = () => {
         <div className="empty-space"></div>
       </div>
       <main className="main-content">
+        {renderSelect()}
         <input type="file" id="file" hidden name="file" accept=".pdf" />
         <label htmlFor="file" className="file-input">
           Upload Here
